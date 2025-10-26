3310910b3705f5760f5e157cc049bee6ecdd3f9b
Rishikesh Shinde <rishikeshshinde88@gmail.com>
2025-10-26 13:56:16 +0530

    Corrected error in safe_get_records

--- fair_vehicle_selector_v1.py
+++ fair_vehicle_selector_v1.py
@@ -76,9 +76,9 @@ def load_gsheet_data(client):
     ws_history = get_or_create_ws("History", ["date","ground","players_present","selected_vehicles","message"])
 
     # Read all data once
-    players = [r["Player"] for r in ws_players.safe_get_records(ws_players, "Players")]
+    players = [r["Player"] for r in safe_get_records(ws_players, "Players")]
     time.sleep(0.2)
-    vehicles = [r["Vehicle"] for r in ws_vehicles.safe_get_records(ws_vehicles, "Vehicles")]
+    vehicles = [r["Vehicle"] for r in safe_get_records(ws_vehicles, "Vehicles")]
     time.sleep(0.2)
     vehicle_groups = {r["Vehicle"]: r["Players"].split(", ") for r in safe_get_records(ws_groups, "VehicleGroups")}
     time.sleep(0.2)
